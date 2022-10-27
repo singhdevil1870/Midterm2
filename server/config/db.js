9a1718e17a0843ba545322385fe3ff1901bdb11a
singhdevil1870 <asin1870@my.centennialcollege.ca>
2022-10-27 17:52:09 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //URI: "mongodb://127.0.0.1/emp",
+ // URI: "mongodb+srv://fawad:XkQKlxVWJiwyHszj@cluster0.8s64hmk.mongodb.net/Emp?retryWrites=true&w=majority",
+  URI:"mongodb+srv://Midterm:midterm9681@cluster0.xx5fzzb.mongodb.net/Emp?retryWrites=true&w=majority",
+};
